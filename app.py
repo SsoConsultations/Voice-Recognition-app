64a0aaa237b4fd30bb22659fd4d2d15f828d119d
SsoConsultations <ssoconsultants14@gmail.com>
2025-07-26 11:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -347,7 +347,6 @@ def logout():
     if 'recorded_samples_count' in st.session_state: del st.session_state.recorded_samples_count
     if 'temp_audio_files' in st.session_state: del st.session_state.temp_audio_files
     if 'current_sample_processed' in st.session_state: del st.session_state.current_sample_processed
-    st.rerun() # Rerun to go back to login page
 
 # Display Logout button if logged in
 if st.session_state.logged_in_as:
